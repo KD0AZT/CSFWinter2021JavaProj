48ad9d890c33bff290476564b5694ec48883173e
KD0AZT <kd0azt@gmail.com>
2021-02-25 13:16:11 -0800

    Added base classes to User.java

--- Sudoku/src/com/sudoku/models/User.java
+++ Sudoku/src/com/sudoku/models/User.java
@@ -1,5 +1,40 @@
 package com.sudoku.models;
+import java.util.Scanner;
 
 public class User {
-
+    
+    String userName;
+    int userScore;
+    
+    private setName() {
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter Username: ");
+        userName = keyboard.next();
+        keyboard.close();
+        
+    }
+    
+    public String getName() {
+        
+        return userName;
+        
+    }
+    
+    private setScore() {
+        
+        userScore++;
+        
+    }
+    
+    public int getScore() {
+    
+        return userScore;
+    
+    }
+    
+    public movesLeft() {
+    
+    }
+    
 }
